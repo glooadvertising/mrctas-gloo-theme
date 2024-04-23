8e4d367fadd30e4619fdeb6c58246beee0322d6b
Pablo Sazo <pablo@glooadvertising.com.au>
2024-04-23 14:20:26 +1000

    token removed

--- template-parts/newsletter-form.php
+++ template-parts/newsletter-form.php
@@ -1,10 +1,10 @@
 <section id="subscribe" class="bg-mrct-ochre-light full py-24 flex justify-center">
     <div class="w-blog flex flex-col gap-4 text-center px-6 md:px-12">
         <h2 class="text-mrct-navy-light text-2xl font-extrabold">Subscribe to our newsletter</h2>
-        <form id="wpforms-form-16" action="/?wpforms_form_preview=16&new_window=1" data-token="aeb2e6480ecb0d66e97491a40126cd03" data-id="16" class="flex flex-col md:flex-row md:justify-center gap-4 wpforms-validate wpforms-form wpforms-ajax-form">
-            <input type="text" name="wpforms[fields][1]" id="wpforms-16-field_1" placeholder="first name" class="bg-transparent border-mrct-navy-light border-2 py-2 px-4 rounded-full placeholder:text-mrct-navy-light">
-            <input type="email" name="wpforms[fields][2]" id="wpforms-16-field_2" placeholder="enter your email" class="bg-transparent border-mrct-navy-light border-2 py-2 px-4 rounded-full placeholder:text-mrct-navy-light">
-            <input type="submit" value="subscribe" name="wpforms[submit]" id="wpforms-submit-16" class="btn btn-lilac flex justify-center">
+        <form id="" action="" class="flex flex-col md:flex-row md:justify-center gap-4">
+            <input type="text" name="" id="" placeholder="first name" class="bg-transparent border-mrct-navy-light border-2 py-2 px-4 rounded-full placeholder:text-mrct-navy-light">
+            <input type="email" name="" id="" placeholder="enter your email" class="bg-transparent border-mrct-navy-light border-2 py-2 px-4 rounded-full placeholder:text-mrct-navy-light">
+            <input type="submit" value="subscribe" name="" id="" class="btn btn-lilac flex justify-center">
         </form>
         <?php echo do_shortcode('[wpforms id="16"]'); ?>
     </div>
