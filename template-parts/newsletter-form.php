9bc62a3ddcfc7ef50e9fcfe145e077b2019bb7b9
Pablo Sazo <pablo@glooadvertising.com.au>
2024-05-29 10:18:17 +1000

    mailchimp

--- template-parts/newsletter-form.php
+++ template-parts/newsletter-form.php
@@ -1,10 +1,14 @@
 <section id="subscribe" class="bg-mrct-ochre-light full py-24 flex justify-center">
     <div class="w-blog flex flex-col gap-4 text-center px-6 md:px-12">
         <h2 class="text-mrct-navy-light text-2xl font-extrabold">Subscribe to our newsletter</h2>
-        <form id="" action="" class="flex flex-col md:flex-row md:justify-center gap-4">
-            <input type="text" name="" id="" placeholder="first name" class="bg-transparent border-mrct-navy-light border-2 py-2 px-4 rounded-full placeholder:text-mrct-navy-light">
-            <input type="email" name="" id="" placeholder="enter your email" class="bg-transparent border-mrct-navy-light border-2 py-2 px-4 rounded-full placeholder:text-mrct-navy-light">
-            <input type="submit" value="subscribe" name="" id="" class="btn btn-lilac flex justify-center">
+        <form id="" action="https://mrctas.us9.list-manage.com/subscribe/post" method="POST" class="flex flex-col md:flex-row md:justify-center gap-4">
+            <input type="hidden" name="u" value="246cba91041aa8cfc5421e519">
+            <input type="hidden" name="id" value="9b0f423988">
+            <input type="text" name="MERGE1" id="MERGE1" placeholder="first name" class="bg-transparent border-mrct-navy-light border-2 py-2 px-4 rounded-full placeholder:text-mrct-navy-light">
+            <input type="text" name="MERGE2" id="MERGE2" placeholder="first name" class="hidden bg-transparent border-mrct-navy-light border-2 py-2 px-4 rounded-full placeholder:text-mrct-navy-light">
+            <input type="text" name="MERGE4" id="MERGE4" placeholder="first name" class="hidden bg-transparent border-mrct-navy-light border-2 py-2 px-4 rounded-full placeholder:text-mrct-navy-light">
+            <input type="email" name="MERGE0" id="MERGE0" placeholder="enter your email" class="bg-transparent border-mrct-navy-light border-2 py-2 px-4 rounded-full placeholder:text-mrct-navy-light">
+            <input type="submit" value="subscribe" name="submit" id="" class="btn btn-lilac flex justify-center">
         </form>
         <!--?php echo do_shortcode('[wpforms id="16"]'); ?-->
     </div>
